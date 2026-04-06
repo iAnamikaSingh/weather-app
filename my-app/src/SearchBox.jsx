07176651ce7946261e3c50bceba9a5d14a62e948
Anamika Singh <214621661+iAnamikaSingh@users.noreply.github.com>
2026-04-06 18:27:01 +0530

    deployed backend url added

--- my-app/src/SearchBox.jsx
+++ my-app/src/SearchBox.jsx
@@ -10,7 +10,7 @@ export default function SearchBox({updateInfo}) {
     const getWeatherInfo = async () => {
         try {
             const response = await fetch(
-                `http://localhost:5000/weather?city=${encodeURIComponent(city.trim())}`
+                `https://weather-app-buzn.onrender.com/weather?city=${encodeURIComponent(city.trim())}`
             );
             const jsonResponse = await response.json();
             if (!response.ok || jsonResponse.error ) {
--- my-app/src/WeatherApp.jsx
+++ my-app/src/WeatherApp.jsx
@@ -17,7 +17,7 @@ export default function WeatherApp () {
     const fetchDelhiWeather = async () => {
         try {
             const response = await fetch(
-                `http://localhost:5000/weather?city=${encodeURIComponent("Delhi".trim())}`
+                `https://weather-app-buzn.onrender.com/weather?city=${encodeURIComponent("Delhi".trim())}`
             );
 
             const data = await response.json();
